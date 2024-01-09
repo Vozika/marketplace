a72d97d863574f21f3a36d22e9ee5207d4a6f810
Sergey Vozika <metasoma@yandex.ru>
2024-01-10 00:32:07 +0300

    add MaxWidthWrapper component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper/MaxWidthWrapper";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <MaxWidthWrapper>
+        <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl"></div>
+      </MaxWidthWrapper>
+    </div>
+  );
 }
